2e802f0c25620d30bacd2a3dc3d9a9155821ba4b
UrielNunez <Devuriel1@gmail.com>
2025-07-04 14:55:16 -0600

    Update: Object Place on touch screen

--- WEBXR/main.js
+++ WEBXR/main.js
@@ -6,18 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
     const initialize = async () => {
 
         //BOTTON AR PERSONALIZADO 
-        const arButton = document.getElementById("ar-button");
+        {
+            const arButton = document.getElementById("ar-button");
 
-        const supported = navigator.xr && navigator.xr.isSessionSupported("immersive-ar");
-        if (!supported) {
-            arButton.textContent = "Not Supported";
-            arButton.disable = true;
-            return;
-        } 
+            const supported = navigator.xr && navigator.xr.isSessionSupported("immersive-ar");
+            if (!supported) {
+                arButton.textContent = "Not Supported";
+                arButton.disable = true;
+                return;
+            }
+        }
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera();
-        const renderer = new THREE.WebGLRenderer({ alpha: true });
+        const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.setPixelRatio(window.devicePixelRatio);
@@ -26,61 +28,75 @@ document.addEventListener('DOMContentLoaded', () => {
         const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
         scene.add(light);
 
-        const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
-        const material = new THREE.MeshBasicMaterial({ color: 0xffbff });
-        const mesh = new THREE.Mesh(geometry, material);
+        //Modelo 3D creado
+        {
+            /*
+            const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+            const material = new THREE.MeshBasicMaterial({ color: 0xffbff });
+            const mesh = new THREE.Mesh(geometry, material);
 
-        mesh.position.set(0, 0, -0.3);
-        scene.add(mesh);
+            mesh.position.set(0, 0, -0.3);
+            scene.add(mesh);
+            */
+        }
 
-       
         //EVENTOS DE SELECCION DE PANTALLA
-        const controller = renderer.xr.getController(0);
+        {
+            const controller = renderer.xr.getController(0);
+            scene.add(controller);
+            /*
+            const events = document.getElementById('events')
+            controller.addEventListener("selectstart", () => {
+                events.prepend("select start ")
+            });
+            controller.addEventListener("selectend", () => {
+                events.prepend("select end ")
+            });*/
 
-        const events = document.getElementById('events')
-        controller.addEventListener("selectstart", () =>{
-            events.prepend("select start ")
-        });
-        controller.addEventListener("selectend", () =>{
-            events.prepend("select end ")
-        });
-        controller.addEventListener("select", () =>{
-            events.prepend("select ")
-        });
+            controller.addEventListener("select", () => {
+                const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+                const material = new THREE.MeshBasicMaterial({ color: 0xffbff * Math.random() });
+                const mesh = new THREE.Mesh(geometry, material);
 
-        
-
-        //BOTON AR PERSONALIZADO
-
-        let currentSession = null
-        const start = async() => {
-            currentSession = await navigator.xr.requestSession("immersive-ar", {optionalFeatures: ['dom-overlay'], domOverlay: {root: document.body}});
-
-            renderer.xr.enabled = true;
-            renderer.xr.setReferenceSpaceType('local');
-            await renderer.xr.setSession(currentSession);
-
-            arButton.textContent = "End";
-
-            renderer.setAnimationLoop( () => {
-                renderer.render(scene,camera);
+                mesh.position.applyMatrix4(controller.matrixWorld);
+                mesh.quaternion.setFromRotationMatrix(controller.matrixWorld);
+                scene.add(mesh);
             });
         }
-        const end = async() =>{
-            currentSession.end();
-            renderer.clear();
-            renderer.setAnimationLoop(null);
 
-            arButton.style.display = "none";
-        }
-        arButton.addEventListener("click", () =>{
-            if (currentSession) {
-                end();
-            } else {
-                start();
+
+        //seccion logica de BOTON AR PERSONALIZADO
+        {
+            let currentSession = null
+            const start = async () => {
+                currentSession = await navigator.xr.requestSession("immersive-ar", { optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } });
+
+                renderer.xr.enabled = true;
+                renderer.xr.setReferenceSpaceType('local');
+                await renderer.xr.setSession(currentSession);
+
+                arButton.textContent = "End";
+
+                renderer.setAnimationLoop(() => {
+                    renderer.render(scene, camera);
+                });
             }
-        });
-        
+            const end = async () => {
+                currentSession.end();
+                renderer.clear();
+                renderer.setAnimationLoop(null);
+
+                arButton.style.display = "none";
+            }
+            arButton.addEventListener("click", () => {
+                if (currentSession) {
+                    end();
+                } else {
+                    start();
+                }
+            });
+        }
+
     }
     initialize();
 });
