d4b73656572d1026076a491d87cd81f87779a395
UrielNunez <Devuriel1@gmail.com>
2025-07-04 19:09:43 -0600

    ToProject separation

---
+++ WEBXR/StartMoodel/main.js
@@ -0,0 +1,76 @@
+import * as THREE from 'three';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const initialize = async () => {
+
+        //BOTTON AR PERSONALIZADO 
+        const arButton = document.getElementById("ar-button");
+        {
+            const supported = navigator.xr && navigator.xr.isSessionSupported("immersive-ar");
+            if (!supported) {
+                arButton.textContent = "Not Supported";
+                arButton.disable = true;
+                return;
+            }
+        }
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
+        const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        document.body.appendChild(renderer.domElement);
+
+        const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+        scene.add(light);
+
+        //Modelo 3D creado
+        {
+            
+            const geometry = new THREE.BoxGeometry(0.06, 0.06, 0.06);
+            const material = new THREE.MeshBasicMaterial({ color: 0xffbff });
+            const mesh = new THREE.Mesh(geometry, material);
+
+            mesh.position.set(0, 0, -0.3);
+            scene.add(mesh);
+            
+        }
+
+
+        //seccion logica de BOTON AR PERSONALIZADO
+        let currentSession = null
+        {
+            const start = async () => {
+                currentSession = await navigator.xr.requestSession("immersive-ar", { optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } });
+
+                renderer.xr.enabled = true;
+                renderer.xr.setReferenceSpaceType('local');
+                await renderer.xr.setSession(currentSession);
+
+                arButton.textContent = "End";
+
+                renderer.setAnimationLoop(() => {
+                    renderer.render(scene, camera);
+                });
+            }
+            const end = async () => {
+                currentSession.end();
+                renderer.clear();
+                renderer.setAnimationLoop(null);
+
+                arButton.style.display = "none";
+            }
+            arButton.addEventListener("click", () => {
+                if (currentSession) {
+                    end();
+                } else {
+                    start();
+                }
+            });
+        }
+
+    }
+    initialize();
+});
